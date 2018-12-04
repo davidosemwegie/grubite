4335ed7c1ca38e15537b7b73af6166a4e7ef3778
David Osemwegie <osahonnn@gmail.com>
2018-12-03 22:15:43 -0800

    a text it returned if there are food items in the category

--- grubite_web/src/components/Dashboard/MenuTable/Table.jsx
+++ grubite_web/src/components/Dashboard/MenuTable/Table.jsx
@@ -3,6 +3,8 @@ import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+import { isEmpty } from '../../../backend/functions'
+import './Table.css'
 
 
 const columns = [
@@ -29,11 +31,22 @@ const columns = [
 class Table extends Component {
 
     render() {
-        return (
-            <div>
-                <BootstrapTable keyField='foodId' data={this.props.data} columns={columns} filter={filterFactory()} />
-            </div>
-        );
+
+        let empty = null
+
+        if (isEmpty(this.props.data)) {
+            return (
+                <p className="emptyFoodTableText">There are no food Items in this category</p>
+            )
+            //empty = (<p className="emptyFoodTableText">There are no food Items in this category</p>) 
+        } else {
+            return (
+                <div>
+                    <BootstrapTable keyField='foodId' data={this.props.data} columns={columns} filter={filterFactory()} />
+                    {empty}
+                </div>
+            )
+        }
     }
 }
 
